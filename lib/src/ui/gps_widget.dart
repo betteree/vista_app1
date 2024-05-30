b36e2d9518a51d5b9a8c7e11d1983006b9438576
나은 <kkne1017@gmail.com>
2024-05-31 01:00:49 +0900

    Update gps_widget.dart

--- lib/src/ui/gps_widget.dart
+++ lib/src/ui/gps_widget.dart
@@ -17,29 +17,39 @@ class RealtimeGPSPage extends StatefulWidget {
 }
 
 class _RealtimeGPSPageState extends State<RealtimeGPSPage> {
-  var _style = HomeWidgetTheme();  // 테마
+  var _style = HomeWidgetTheme(); // 테마
   final maxWidth = 400.0;
   final maxHeight = 900.0;
   bool interaction = false;
 
   @override
   Widget build(BuildContext context) {
-    double queryWidth = MediaQuery.of(context).size.width;
+    double queryWidth = MediaQuery
+        .of(context)
+        .size
+        .width;
     // 가로 최대 길이를 400으로 한정
-    if (queryWidth > maxWidth) {queryWidth = maxWidth;}
-    double queryHeight = MediaQuery.of(context).size.height;
+    if (queryWidth > maxWidth) {
+      queryWidth = maxWidth;
+    }
+    double queryHeight = MediaQuery
+        .of(context)
+        .size
+        .height;
     // 세로 최대 길이를 1200으로  한정
-    if (queryHeight> maxHeight) {queryHeight= maxHeight; }
+    if (queryHeight > maxHeight) {
+      queryHeight = maxHeight;
+    }
 
     return BlocBuilder<SocketBloc, SocketState>(
-        builder: (context, state){
+        builder: (context, state) {
           print("하이");
-          if(state is RealtimeGPSState){
+          if (state is RealtimeGPSState) {
             print("x값 ${state.x}");
             return Scaffold(
-                body : GoogleMapWidget(queryWidth, queryHeight, state)
+                body: GoogleMapWidget(queryWidth, queryHeight, state)
             );
-          }else{
+          } else {
             return Container(
                 width: queryWidth,
                 height: queryHeight,
@@ -50,9 +60,31 @@ class _RealtimeGPSPageState extends State<RealtimeGPSPage> {
         }
     );
   }
-  Widget GoogleMapWidget(width, height, state) {
+
+  Widget GoogleMapWidget(double width, double height,state) {
     LatLng initialLocation = LatLng(state.y, state.x);
 
+    // 마커 생성
+    final Marker marker = Marker(
+      markerId: MarkerId('where'),
+      position: initialLocation,
+    );
+
+    // 원 생성
+    final Circle circle = Circle(
+      circleId: CircleId('circle'),
+      center: initialLocation,
+      // 원의 중심이 되는 위치
+      fillColor: Colors.blue.withOpacity(0.5),
+      // 원의 색상
+      radius: 10,
+      // 원의 반지름 (미터 단위)
+      strokeColor: Colors.blue,
+      // 원의 테두리 색
+      strokeWidth: 1, // 원의 두께
+    );
+
+    // 마커와 원을 포함한 지도 위젯 반환
     return Container(
       width: width,
       height: height,
@@ -62,6 +94,10 @@ class _RealtimeGPSPageState extends State<RealtimeGPSPage> {
           target: initialLocation,
           zoom: 17.0,
         ),
+        markers: Set<Marker>.of([marker]),
+        // 마커 추가
+        circles: Set<Circle>.of([circle]),
+        // 원 추가
         onMapCreated: (GoogleMapController controller) {
           // 이곳에 GoogleMapController를 받아오는 코드가 들어갑니다.
           // 이 함수에서는 받아온 controller를 사용하여 지도를 제어할 수 있습니다.
@@ -69,5 +105,5 @@ class _RealtimeGPSPageState extends State<RealtimeGPSPage> {
       ),
     );
   }
+}
 
-  }
